f21b12b72b310c38103582d28a31bf0f7aaa7533
Dan Herrera <dan@revelationglobal.com>
2011-06-10 10:36:58 -0700

    testing out java2doc

--- Rakefile
+++ Rakefile
@@ -5,11 +5,5 @@ require 'bundler'
 Bundler.setup
 
 Rake::TestTask.new do |t|
-  #ENV['JAVA_OPTS'] = "-d32"
   t.pattern = "spec/*_spec.rb"
 end
-
-#task :default do
-#  `JAVA_OPTS="-d32" bundle exec ruby ./spec/junodoc_spec.rb`
-#  `JAVA_OPTS="-d32" rake test`
-#end
--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -7,38 +7,32 @@ describe JunoDoc::Document do
 
   it "should be an instance of JunoDoc::Document" do
     @junodoc.class.must_equal(JunoDoc::Document)
-    @junodoc.close_document
   end
 
   it "should be able to set the name of the document" do
     @junodoc.name = "what's_up.doc"
     @junodoc.name.must_equal("what's_up.doc")
-    @junodoc.close_document
   end
 
   it "should be able to add text to the document" do
     @junodoc.add_text("istanbul, not constantinople")
-    @junodoc.close_document
   end
 
   it "should be able to add a paragraph break" do
     @junodoc.add_text("istanbul, not constantinople")
     @junodoc.add_paragraph_break
-    @junodoc.close_document
   end
 
   it "should be able to add an image by url" do
     @junodoc.add_text("istanbul, not constantinople")
     @junodoc.add_paragraph_break
     @junodoc.add_image("file://#{Dir.pwd}/spec/fixtures/image.jpg")
-    @junodoc.close_document
   end
 
   it "should be able to write out the document to disk" do
     @junodoc.add_text("istanbul, not constantinople")
     @junodoc.add_paragraph_break
     @junodoc.add_image("file://#{Dir.pwd}/spec/fixtures/image.jpg")
-    @junodoc.write_document(Dir.pwd + '/spec/output/test.doc')
-    @junodoc.close_document
+    @junodoc.write_document(Dir.pwd + '/spec/output/')
   end
 end
